2e81f8e0624e7755f4d41822a9127c203a778d30
XTroleX <kacper.manka32123@gmail.com>
2026-03-17 23:59:20 +0100

    Skrypt PHP

--- pliki3/medica.php
+++ pliki3/medica.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="pl">
+      <?php
+        $polaczenie = mysqli_connect("localhost","root","","medica");
+    ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,31 +15,62 @@
         <h1>Abonamenty w przychodni Medica</h1>
     </header>
     <article>
+        <?php
+            $zap1 = "SELECT abonamenty.nazwa, abonamenty.cena, abonamenty.opis FROM abonamenty;";
+            $wynik =  mysqli_query($polaczenie,$zap1);
+            if($wynik){
+                while ($row = mysqli_fetch_array($wynik)){
+                    echo "<h3>Pakiet $row[0] - cena $row[1] zł</h3>";
+                    echo "<p>$row[2]</p>";
+                }
+            }
+        ?>
 <a href="opis.html">Dowiedz się więcej</a>
     </article>
     <main>
         <section>
     <h2>Standardowy</h2>
     <ul>
-        <li>
-
-        </li>
+        <?php
+                    $zap3 = "SELECT abonamenty.nazwa, cechy.cecha FROM abonamenty INNER JOIN szczegolyabonamentu ON abonamenty.id = szczegolyabonamentu.Abonamenty_id INNER JOIN cechy ON szczegolyabonamentu.Cechy_id = cechy.id WHERE abonamenty.id = 1;";
+                    $wynik =  mysqli_query($polaczenie,$zap3);
+                    
+                    while ($row = mysqli_fetch_array($wynik)){
+                    echo "<li>$row[1]</li>";
+                    
+                    }
+                    
+                ?>
     </ul>
         </section>
         <section>
             <h2>Premium</h2>
  <ul>
-        <li>
-
-        </li>
+        <?php
+                    $zap3 = "SELECT abonamenty.nazwa, cechy.cecha FROM abonamenty INNER JOIN szczegolyabonamentu ON abonamenty.id = szczegolyabonamentu.Abonamenty_id INNER JOIN cechy ON szczegolyabonamentu.Cechy_id = cechy.id WHERE abonamenty.id = 2;";
+                    $wynik =  mysqli_query($polaczenie,$zap3);
+                    
+                    while ($row = mysqli_fetch_array($wynik)){
+                    echo "<li>$row[1]</li>";
+                    
+                    }
+                    
+                ?>
     </ul>
         </section>
         <section>
             <h2>Dziecko</h2>
  <ul>
-        <li>
-
-        </li>
+        <?php
+                    $zap3 = "SELECT abonamenty.nazwa, cechy.cecha FROM abonamenty INNER JOIN szczegolyabonamentu ON abonamenty.id = szczegolyabonamentu.Abonamenty_id INNER JOIN cechy ON szczegolyabonamentu.Cechy_id = cechy.id WHERE abonamenty.id = 3;";
+                    $wynik =  mysqli_query($polaczenie,$zap3);
+                    
+                    while ($row = mysqli_fetch_array($wynik)){
+                    echo "<li>$row[1]</li>";
+                    
+                    }
+                    
+                ?>
     </ul>
         </section>
     </main>
@@ -46,5 +80,8 @@
             Stronę przygotował: 000000000000
         </p>
     </footer>
+    <?php
+        mysqli_close($polaczenie);
+    ?>
 </body>
 </html>
