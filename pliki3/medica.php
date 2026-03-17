e44cd9db271e79e86df46aca77ab415b3dfaa6a7
XTroleX <kacper.manka32123@gmail.com>
2026-03-17 23:28:45 +0100

    CSS

--- pliki3/medica.php
+++ pliki3/medica.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Przychodnia Medica</title>
     <link rel="icon" href="obraz2.png">
-    <link rel="stylesheet" href="styl.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
