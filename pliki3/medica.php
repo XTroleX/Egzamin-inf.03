86c375468d7cb5567301be442cd70125582348f4
XTroleX <kacper.manka32123@gmail.com>
2026-03-17 23:20:15 +0100

    HTML

---
+++ pliki3/medica.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Przychodnia Medica</title>
+    <link rel="icon" href="obraz2.png">
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Abonamenty w przychodni Medica</h1>
+    </header>
+    <article>
+<a href="opis.html">Dowiedz się więcej</a>
+    </article>
+    <main>
+        <section>
+    <h2>Standardowy</h2>
+    <ul>
+        <li>
+
+        </li>
+    </ul>
+        </section>
+        <section>
+            <h2>Premium</h2>
+ <ul>
+        <li>
+
+        </li>
+    </ul>
+        </section>
+        <section>
+            <h2>Dziecko</h2>
+ <ul>
+        <li>
+
+        </li>
+    </ul>
+        </section>
+    </main>
+    <footer>
+        <p>
+            <img src="obraz2.png" alt="przychodnia">
+            Stronę przygotował: 000000000000
+        </p>
+    </footer>
+</body>
+</html>
